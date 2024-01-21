25d7aaf056994e13caa6ddee86c8ba1da996d9eb
Faisal Rahman <Faisalrahman6026@gmail.com>
2024-01-22 02:59:40 +0600

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,23 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int age = 25;
+        double salary = 50000;
+        String name = "Faisal";
+        int number = 10;
+
+        if (number>0)
+        {
+            System.out.println("positive number");
+        }else if (number<0)
+    {
+            System.out.println("Negative Number");
+        }
+        else
+        {
+            System.out.println("Zero");
+        }
+        {
+            System.out.println(age);
+        }
+    }
+}
